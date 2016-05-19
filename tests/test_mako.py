293d687187674f8791c537945c3ea0501a1dae28
Blaise Laflamme <blaise@laflamme.org>
2016-05-18 23:57:02 -0400

    add override tests

--- tests/test_mako.py
+++ tests/test_mako.py
@@ -3,7 +3,9 @@ from webtest import TestApp as Client
 
 from .fixtures import (
     template,
-    template_inheritance
+    template_inheritance,
+    template_override,
+    template_inheritance_override
     )
 
 
@@ -32,6 +34,54 @@ def test_template_inheritance():
 <html>
 <body>
 <div id="content">
+\n<p>Hello world!</p>\n
+</div>
+</body>
+</html>\n'''
+
+
+def test_template_override():
+    c = Client(template_override.App())
+
+    response = c.get('/persons/world')
+    assert response.body == b'''\
+<html>
+<body>
+<p>Hello world!</p>
+</body>
+</html>\n'''
+
+    c = Client(template_override.SubApp())
+
+    response = c.get('/persons/world')
+    assert response.body == b'''\
+<html>
+<body>
+<p>Hi world!</p>
+</body>
+</html>\n'''
+
+
+def test_template_inheritance_override():
+    c = Client(template_inheritance_override.App())
+
+    response = c.get('/persons/world')
+    assert response.body == b'''\
+<html>
+<body>
+<div id="content">
+\n<p>Hello world!</p>\n
+</div>
+</body>
+</html>\n'''
+
+    c = Client(template_inheritance_override.SubApp())
+
+    response = c.get('/persons/world')
+    assert response.body == b'''\
+<html>
+<body>
+<div id="content2">
 \n<p>Hello world!</p>\n
 </div>
 </body>
