e123d6551a64e5eed89f58cd92c307bce2964a43
Blaise Laflamme <blaise@laflamme.org>
2016-05-18 23:56:51 -0400

    add fixture apps

---
+++ tests/fixtures/template_override.py
@@ -0,0 +1,35 @@
+import morepath
+from more.mako import MakoApp
+
+
+class App(MakoApp):
+    pass
+
+
+@App.path(path='persons/{name}')
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+
+
+@App.template_directory()
+def get_template_dir():
+    return 'templates'
+
+
+@App.html(model=Person, template='person.mako')
+def person_default(self, request):
+    return {'name': self.name}
+
+
+class SubApp(App):
+    pass
+
+
+@SubApp.template_directory()
+def get_template_dir_override():
+    return 'templates_override'
+
+
+if __name__ == '__main__':
+    morepath.run(App())
